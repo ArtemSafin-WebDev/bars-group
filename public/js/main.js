33e6e0e718ebd37631cbe1d27b8cb97dfde7949c
Ildar Meyker <ildar.meyker@gmail.com>
2019-07-02 18:11:24 +0300

    Add input-attach-file. Refactor icons.

--- public/js/main.js
+++ public/js/main.js
@@ -1076,6 +1076,26 @@ var Form = {
 		$(this).removeClass('_hover');
 	},
 
+	_handleFileChange: function (e) {
+		var self = e.data.self;
+
+		var files = $(this).find('input[type="file"]')[0].files;
+		var names = $.map(files, function (file) {
+			return file.name;
+		});
+
+		if ( names.length == 0 ) {
+			$(this)
+				.removeClass('_chosen');
+		} else {
+			$(this)
+				.addClass('_chosen')
+				.find('.js-form-file__names')
+				.html(names.join(', '));
+		}
+
+	},
+
 	_bindUI: function () {
 		var self = this;
 
@@ -1083,6 +1103,7 @@ var Form = {
 		$(document).on('blur', '.js-form-input', {self: self}, self._handleBlurOnInput);
 		$(document).on('mouseover', '.js-form-wings', {self: self}, self._handleWingsMouseover);
 		$(document).on('mouseout', '.js-form-wings', {self: self}, self._handleWingsMouseout);
+		$(document).on('change', '.js-form-file', {self: self}, self._handleFileChange);
 	},
 
 	init: function () {
