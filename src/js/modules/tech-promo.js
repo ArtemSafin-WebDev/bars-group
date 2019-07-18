affa4bc843f7414753f5f450336a124783390f0a
Ildar Meyker <ildar.meyker@gmail.com>
2019-07-18 18:13:51 +0300

    Add tech-promo progress.

--- src/js/modules/tech-promo.js
+++ src/js/modules/tech-promo.js
@@ -1,6 +1,8 @@
 
 var TechPromo = {
 
+
+
 	_bindUI: function () {
 		var self = this;
 
@@ -13,15 +15,21 @@ var TechPromo = {
 
 		if ( $_.length == 0) return;
 
-		$_.find('.tech-promo__elem-1').addClass('--active');
+		$_.find('.tech-promo__orbit').addClass('--active');
 
 		setTimeout(function () {
 			$_.find('.tech-promo__point').addClass('--active');
 		}, 2000);
 
 		setTimeout(function () {
-			$_.find('.tech-promo__data').addClass('--active');
-		}, 4000);
+			$_.find('.tech-promo__center').addClass('--active');
+		}, 3700);
+
+		setTimeout(function () {
+			$_.find('.tech-promo__circle').addClass('--active');
+		}, 4500);
+
+		self._bindUI();
 	}
 }
 
