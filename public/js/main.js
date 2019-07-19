eeb74784e78964174a1217d5133e603e9138625d
Ildar Meyker <ildar.meyker@gmail.com>
2019-07-19 19:24:52 +0300

    Add video loading.

--- public/js/main.js
+++ public/js/main.js
@@ -1616,11 +1616,48 @@ var NavBanner = {
 
 var TechPromo = {
 
+	_handleCanPlayEvent: function (e) {
+		var self = e.data.self;
 
+		objectFitPolyfill(this);
+		$(this).addClass('--active');
+
+		if ( $(this).closest('.tech-promo__figure__item').index() == 0 ) {
+			$(this)[0].play();
+		}
+	},
+
+	_setActiveVideo: function (index) {
+		var self = this;
+
+		var $items = $('#tech-promo .tech-promo__figure__item');
+		$items.filter('.--active').removeClass('--active').end().find('video')[0].pause();
+		$items.eq(index).addClass('--active').end().find('video')[0].play();
+	},
+
+	_handleCircleEnter: function (e) {
+		var self = e.data.self;
+
+		e.preventDefault();
+
+		var index = $(this).data('index');
+		self._setActiveVideo(index);
+	},
+
+	_handleCircleLeave: function (e) {
+		var self = e.data.self;
+
+		e.preventDefault();
+
+		self._setActiveVideo(0);
+	},
 
 	_bindUI: function () {
 		var self = this;
 
+		$('#tech-promo video').on('canplaythrough', {self: self}, self._handleCanPlayEvent);
+		$('#tech-promo .tech-promo__circle').on('mouseenter', {self: self}, self._handleCircleEnter);
+		$('#tech-promo .tech-promo__circle').on('mouseleave', {self: self}, self._handleCircleLeave);
 	},
 
 	init: function () {
