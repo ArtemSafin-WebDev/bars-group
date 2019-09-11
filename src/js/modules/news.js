43af8c2570d2f436945053c1d7dd603012d40091
Артем Сафин <artemsafin.webdev@gmail.com>
2019-09-11 14:37:30 +0300

    Сделал текст одинакового размера

--- src/js/modules/news.js
+++ src/js/modules/news.js
@@ -7,53 +7,58 @@ module.exports = {
         );
 
         newsSliderContainer.forEach(function(slider) {
-			const mainContentSliderContainer = slider.querySelector('.js-news-slider-container');
-			const mainSliderNext = slider.parentElement.querySelector(
-				".js-news-slider-next"
-			);
-			const mainSliderPrev = slider.parentElement.querySelector(
-				".js-news-slider-prev"
-			);
-			const BGimagesSliderContainer = slider.querySelector('.js-news-bg-images-slider-container');
-            
-			const BGImageSlider = new Swiper(BGimagesSliderContainer, {
-				effect: "fade",
-				loop: true,
-				allowTouchMove: false
-			})
+            const mainContentSliderContainer = slider.querySelector(
+                ".js-news-slider-container"
+            );
+            const mainSliderNext = slider.parentElement.querySelector(
+                ".js-news-slider-next"
+            );
+            const mainSliderPrev = slider.parentElement.querySelector(
+                ".js-news-slider-prev"
+            );
+            const BGimagesSliderContainer = slider.querySelector(
+                ".js-news-bg-images-slider-container"
+            );
 
-			const mainContentSlider = new Swiper(mainContentSliderContainer, {
-                slidesPerView: "auto",
+            const BGImageSlider = new Swiper(BGimagesSliderContainer, {
+                effect: "fade",
+                loop: true,
+                allowTouchMove: false
+            });
+
+            const mainContentSlider = new Swiper(mainContentSliderContainer, {
+				slidesPerView: "auto",
                 spaceBetween: 60,
-				loop: true,
-				slideToClickedSlide: true,
+                loop: true,
+                slideToClickedSlide: true,
                 breakpoints: {
                     1050: {
                         spaceBetween: 40
                     }
-				},
-				thumbs: {
-					swiper: BGImageSlider
-				},
-				navigation: {
-					nextEl: mainSliderNext,
-					prevEl: mainSliderPrev
-				}
-			});
+                },
+                thumbs: {
+                    swiper: BGImageSlider
+                },
+                navigation: {
+                    nextEl: mainSliderNext,
+                    prevEl: mainSliderPrev
+                },
+            });
 
+           
 
-			// mainSliderNext.addEventListener('mouseenter', function() {
-			// 	slider.classList.add('images-shown');
-			// })
-			// mainSliderPrev.addEventListener('mouseenter', function() {
-			// 	slider.classList.add('images-shown');
-			// })
-			// slider.addEventListener('mouseenter', function() {
-			// 	slider.classList.add('images-shown');
-			// })
-			// slider.addEventListener('mouseleave', function() {
-			// 	slider.classList.remove('images-shown');
-			// })
+            // mainSliderNext.addEventListener('mouseenter', function() {
+            // 	slider.classList.add('images-shown');
+            // })
+            // mainSliderPrev.addEventListener('mouseenter', function() {
+            // 	slider.classList.add('images-shown');
+            // })
+            // slider.addEventListener('mouseenter', function() {
+            // 	slider.classList.add('images-shown');
+            // })
+            // slider.addEventListener('mouseleave', function() {
+            // 	slider.classList.remove('images-shown');
+            // })
         });
     }
 };
