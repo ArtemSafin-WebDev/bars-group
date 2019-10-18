baa7ec6eefb7ef280cbd8982fa6e656217e1b749
Артем Сафин <artemsafin.webdev@gmail.com>
2019-10-18 12:28:34 +0300

    Фикс анимаций на скролл

--- src/js/modules/scrollAnimations.js
+++ src/js/modules/scrollAnimations.js
@@ -1,22 +1,27 @@
 module.exports = {
     init: function() {
-        const blocksToAnimate = Array.prototype.slice.call(
-            document.querySelectorAll(".block-wrapper")
-        );
+
+        const landingMain = document.querySelector('.landing-main');
+
+        if (!landingMain) {
+            console.warn('No landing main');
+            return;
+        }
+        
+        const blocksToAnimate = Array.prototype.slice.call(landingMain.children);
 
         blocksToAnimate.shift();
 
         const options = {
-            threshold: 0.35
+            // threshold: 0.35,
+            rootMargin: "100px 0px 0px 0px"
         };
 
         const observer = new IntersectionObserver(entries => {
             entries.forEach(entry => {
                 if (entry.intersectionRatio > 0) {
                     entry.target.classList.add("revealed");
-                } else {
-                    entry.target.classList.remove("revealed");
-                }
+                } 
             });
         }, options);
 
