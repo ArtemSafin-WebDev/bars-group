c4b6ee5dea8a6e41ff8a72845c5efb6fdefae7e9
Артем Сафин <artemsafin.webdev@gmail.com>
2019-12-02 15:04:22 +0300

    Теоретический фикс видео

--- src/js/modules/app.js
+++ src/js/modules/app.js
@@ -1,184 +1,208 @@
-var $ = require('jquery');
-require('objectFitPolyfill');
+var $ = require("jquery");
+require("objectFitPolyfill");
 
-var TechPromo = require('./techPromo');
-var NavMobile = require('./navMobile');
-var Overview = require('./overview');
-var Catalog = require('./catalog');
-var Hover = require('./hover');
-var NavBanner = require('./navBanner');
-var GanttSlider = require('./ganttSlider');
-var CitiesSlider = require('./citiesSlider');
-var SliderContent = require('./sliderContent');
-var SliderDigits = require('./sliderDigits');
-var SliderTabs = require('./sliderTabs');
-var Header = require('./header');
-var News = require('./news');
-var Form = require('./form');
-var ScrollableTable = require('./scrollableTable');
-var NewsSlider = require('./newsSlider');
-var NewsPhotoSlider = require('./newsPhotoSlider');
-var NewsToggles = require('./newsToggles');
-var NavSticker = require('./navSticker');
-var About = require('./about');
-var TAdvantages = require('./tAdvantages');
-var TSliders = require('./tSliders');
-var Utils = require('./utils');
-var Popup = require('./popup');
-var Arch = require('./arch');
-var NavFilter = require('./navFilter');
-var Lazyload = require('./lazyload');
-var TasksSliders = require('./tTasks');
-var AdvantagesSliders = require('./advantagesSlider');
-var ScrollAnimations = require('./scrollAnimations');
-var AboutNew = require('./aboutNew');
-var Architecture = require('./architectureSlider');
-var WasIntegrated = require('./wasIntegrated');
-var DownloadBtns = require('./downloadBtn');
-var NewAdvantagesSlider = require('./newAdvantagesSlider');
-var NavBannerScrolled = require('./navBannerScrolled');
-var CircleAccordeons = require('./circleAccordeons');
+var TechPromo = require("./techPromo");
+var NavMobile = require("./navMobile");
+var Overview = require("./overview");
+var Catalog = require("./catalog");
+var Hover = require("./hover");
+var NavBanner = require("./navBanner");
+var GanttSlider = require("./ganttSlider");
+var CitiesSlider = require("./citiesSlider");
+var SliderContent = require("./sliderContent");
+var SliderDigits = require("./sliderDigits");
+var SliderTabs = require("./sliderTabs");
+var Header = require("./header");
+var News = require("./news");
+var Form = require("./form");
+var ScrollableTable = require("./scrollableTable");
+var NewsSlider = require("./newsSlider");
+var NewsPhotoSlider = require("./newsPhotoSlider");
+var NewsToggles = require("./newsToggles");
+var NavSticker = require("./navSticker");
+var About = require("./about");
+var TAdvantages = require("./tAdvantages");
+var TSliders = require("./tSliders");
+var Utils = require("./utils");
+var Popup = require("./popup");
+var Arch = require("./arch");
+var NavFilter = require("./navFilter");
+var Lazyload = require("./lazyload");
+var TasksSliders = require("./tTasks");
+var AdvantagesSliders = require("./advantagesSlider");
+var ScrollAnimations = require("./scrollAnimations");
+var AboutNew = require("./aboutNew");
+var Architecture = require("./architectureSlider");
+var WasIntegrated = require("./wasIntegrated");
+var DownloadBtns = require("./downloadBtn");
+var NewAdvantagesSlider = require("./newAdvantagesSlider");
+var NavBannerScrolled = require("./navBannerScrolled");
+var CircleAccordeons = require("./circleAccordeons");
 
-require('./scrollbox');
+require("./scrollbox");
 
 module.exports = {
+    _state: {
+        preloaderTimer: null,
+        promoVideosTotal: 0,
+        promoVideosLoaded: 0,
+        isUserActivityHandled: false,
+        isWindowLoaded: false
+    },
 
-	_state: {
-		preloaderTimer: null,
-		promoVideosTotal: 0,
-		promoVideosLoaded: 0,
-		isUserActivityHandled: false,
-		isWindowLoaded: false
-	},
+    _showContent: function() {
+        var self = this;
 
-	_showContent: function () {
-		var self = this;
+        if (self._state.isWindowLoaded === false) return;
+        if (self._state.promoVideosLoaded !== self._state.promoVideosTotal)
+            return;
 
-		if ( self._state.isWindowLoaded === false ) return;
-		if ( self._state.promoVideosLoaded !== self._state.promoVideosTotal) return;
+        clearInterval(self._state.preloaderTimer);
 
-		clearInterval(self._state.preloaderTimer);
+        $("#loader-main").removeClass("_active");
+        $("body").removeClass("page__locked");
 
-		$('#loader-main').removeClass('_active');
-		$('body').removeClass('page__locked');
+        TechPromo.init();
+        NavMobile.init();
+        Overview.init();
+        Catalog.init();
+        Hover.init();
+        Popup.init();
+        NavFilter.init();
+        TAdvantages.init();
+        Lazyload.init();
+        GanttSlider.init();
+        CitiesSlider.init();
+        ScrollAnimations.init();
+        AboutNew.init();
+        Architecture.init();
+        WasIntegrated.init();
+        DownloadBtns.init();
+        NewAdvantagesSlider.init();
+        NavBannerScrolled.init();
+        CircleAccordeons.init();
 
-		
-		TechPromo.init();
-		NavMobile.init();
-		Overview.init();
-		Catalog.init();
-		Hover.init();
-		Popup.init();
-		NavFilter.init();
-		TAdvantages.init();
-		Lazyload.init();
-		GanttSlider.init();
-		CitiesSlider.init();
-		ScrollAnimations.init();
-		AboutNew.init();
-		Architecture.init();
-		WasIntegrated.init();
-		DownloadBtns.init();
-		NewAdvantagesSlider.init();
-		NavBannerScrolled.init();
-		CircleAccordeons.init();
+        // trigger click to start loading lazy videos
+        $("body").trigger("click");
+    },
 
-		// trigger click to start loading lazy videos
-		$('body').trigger('click');
-	},
+    _startLazyVideosLoading: function() {
+        var self = this;
 
-	_startLazyVideosLoading: function () {
-		var self = this;
+        $("video[data-lazy]").each(function() {
+            $(this)[0].load();
+        });
+    },
 
-		$('video[data-lazy]').each(function () {
-			$(this)[0].load();
-		});
-	},
+    _handleUserActivity: function(e) {
+        var self = e.data.self;
 
-	_handleUserActivity: function (e) {
-		var self = e.data.self;
+        if (self._state.isUserActivityHandled) return;
+        if (!Modernizr.video || Modernizr.lowbandwidth) return;
 
-		if ( self._state.isUserActivityHandled ) return;
-		if ( !Modernizr.video || Modernizr.lowbandwidth ) return;
+        self._state.isUserActivityHandled = true;
+        setTimeout(self._startLazyVideosLoading.bind(self), 100);
+    },
 
-		self._state.isUserActivityHandled = true;
-		setTimeout(self._startLazyVideosLoading.bind(self), 100);
-	},
+    _handleDOMReady: function() {
+        var self = this;
 
-	_handleDOMReady: function () {
-		var self = this;
+        // it's important to call NavBanner inition first,
+        // because tabs content can contain other sliders inside
+        NavBanner.init();
 
-		// it's important to call NavBanner inition first,
-		// because tabs content can contain other sliders inside
-		NavBanner.init();
+        SliderContent.init();
+        SliderDigits.init();
+        SliderTabs.init();
+        Header.init();
+        News.init();
+        Form.init();
+        ScrollableTable.init();
+        NewsSlider.init();
+        NewsPhotoSlider.init();
+        NewsToggles.init();
+        NavSticker.init();
+        About.init();
+        TSliders.init();
+        Arch.init();
+        TasksSliders.init();
+        AdvantagesSliders.init();
+    },
 
-		
-		SliderContent.init();
-		SliderDigits.init();
-		SliderTabs.init();
-		Header.init();
-		News.init();
-		Form.init();
-		ScrollableTable.init();
-		NewsSlider.init();
-		NewsPhotoSlider.init();
-		NewsToggles.init();
-		NavSticker.init();
-		About.init();
-		TSliders.init();
-		Arch.init();
-		TasksSliders.init();
-		AdvantagesSliders.init();
-		
+    _handleWindowLoad: function() {
+        var self = this;
 
-	},
+        self._state.isWindowLoaded = true;
+    },
 
-	_handleWindowLoad: function () {
-		var self = this;
+    _handleCanPlayEvent: function(e) {
+        var self = e.data.self;
+        console.log("Canplay event happended", e.originalEvent);
 
-		self._state.isWindowLoaded = true;
-	},
+        $(this).addClass("_active");
+        objectFitPolyfill(this);
 
-	_handleCanPlayEvent: function (e) {
-		var self = e.data.self;
+        if ($(this).data("promo") !== undefined) {
+            self._state.promoVideosLoaded++;
+        }
 
+        if ($(this).data("play") !== undefined) {
+            $(this)[0].play();
+        }
+    },
 
-		$(this).addClass('_active');
-		objectFitPolyfill(this);
+    _bindUI: function() {
+        var self = this;
 
-		if ($(this).data('promo') !== undefined) {
-			self._state.promoVideosLoaded++;
-		}
+        $(document).one(
+            "click touchstart",
+            { self: self },
+            self._handleUserActivity
+        );
+        $(window).on("load", self._handleWindowLoad.bind(self));
+        $("video").one(
+            "canplaythrough",
+            { self: self },
+            self._handleCanPlayEvent
+        );
+        Array.prototype.slice
+            .call(document.querySelectorAll("video"))
+            .forEach(function(video) {
+                if (videoElement.readyState > 3) {
+                    console.log("Video in ready state", video);
+                    $(this).addClass("_active");
+                    objectFitPolyfill(this);
 
-		if ($(this).data('play') !== undefined) {
-			$(this)[0].play();
-		} 
-	},
+                    if ($(this).data("promo") !== undefined) {
+                        self._state.promoVideosLoaded++;
+                    }
 
-	_bindUI: function () {
-		var self = this;
+                    if ($(this).data("play") !== undefined) {
+                        $(this)[0].play();
+                    }
+                }
+            });
+        $(self._handleDOMReady.bind(self));
+    },
 
-		$(document).one('click touchstart', {self: self}, self._handleUserActivity);
-		$(window).on('load', self._handleWindowLoad.bind(self));
-		$('video').one('canplaythrough', {self: self}, self._handleCanPlayEvent);
-		$(self._handleDOMReady.bind(self));
-	},
+    init: function() {
+        var self = this;
 
-	init: function () {
-		var self = this;
+        if (Utils.isMobile()) {
+            $("html").addClass("mrkwbr-is-mobile");
+        } else {
+            $("html").addClass("mrkwbr-no-mobile");
+        }
 
-		if (Utils.isMobile()) {
-			$('html').addClass('mrkwbr-is-mobile');
-		} else {
-			$('html').addClass('mrkwbr-no-mobile');
-		}
+        // count promo videos
+        self._state.promoVideosTotal = $("video[data-promo]").length;
 
-		// count promo videos
-		self._state.promoVideosTotal = $('video[data-promo]').length;
+        // run preloader timer
+        self._state.preloaderTimer = setInterval(
+            self._showContent.bind(self),
+            50
+        );
 
-		// run preloader timer
-		self._state.preloaderTimer = setInterval(self._showContent.bind(self), 50);
-
-		self._bindUI();
-	}
+        self._bindUI();
+    }
 };
